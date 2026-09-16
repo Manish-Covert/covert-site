733badd88de674605b36ae32f17a1a2b2770f244
Manish-Covert <80510124+Manish-Covert@users.noreply.github.com>
2026-09-16 12:40:44 +0530

    Assemble New Day Solar case study image asset

---
+++ src/case-study-assets/new-day-solar/index.js
@@ -0,0 +1,5 @@
+import homepage0 from './homepage-00'
+import homepage1 from './homepage-01'
+import homepage2 from './homepage-02'
+
+export const NEW_DAY_SOLAR_HOMEPAGE = `data:image/webp;base64,${homepage0}${homepage1}${homepage2}`
