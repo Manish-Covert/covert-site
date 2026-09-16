8d12e9b5677aa17905bc7bb7952afca2e538c63a
Manish-Covert <80510124+Manish-Covert@users.noreply.github.com>
2026-09-16 17:14:13 +0530

    Replace New Day Solar case study images

--- src/case-study-assets/new-day-solar/index.js
+++ src/case-study-assets/new-day-solar/index.js
@@ -1,12 +1,7 @@
-import homepage0 from './homepage-00'
-import homepage1 from './homepage-01'
-import homepage2 from './homepage-02'
-import localSeo from './local-seo-ultra'
-import crm0 from './crm-00'
-import crm1 from './crm-01'
-import crm2 from './crm-02'
-import crm3 from './crm-05'
+import homepage from './homepage-new'
+import localSeo from './local-seo-new'
+import crm from './crm-new'
 
-export const NEW_DAY_SOLAR_HOMEPAGE = `data:image/webp;base64,${homepage0}${homepage1}${homepage2}`
+export const NEW_DAY_SOLAR_HOMEPAGE = `data:image/webp;base64,${homepage}`
 export const NEW_DAY_SOLAR_LOCAL_SEO = `data:image/webp;base64,${localSeo}`
-export const NEW_DAY_SOLAR_CRM = `data:image/webp;base64,${crm0}${crm1}${crm2}${crm3}`
+export const NEW_DAY_SOLAR_CRM = `data:image/webp;base64,${crm}`
